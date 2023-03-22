4fc09eaf36a4151e14bdc3041c6739709eb5173b
phsiao2000 <phsiao2000@gmail.com>
2023-03-22 00:31:46 +0000

    Added lifetime to enum Order, converted all Strings to str

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::collections::HashMap;
 
-struct Order {
-    name: String,
+struct Order<'a> {
+    name: &'a str,
 }
-fn order_sweets(orders: Vec<Order>, prices: HashMap<String, f64>) -> Option<f64> {
+fn order_sweets(orders: Vec<Order>, prices: HashMap<&str, f64>) -> Option<f64> {
     let mut price = 0.0;
     for order in orders {
         price += prices.get(&order.name)?;
-    } 
+    }
     // let donut_price = prices.get(&orders[0].name)?; // early return if None
     // let cake_price = prices.get(&orders[1].name)?; // early return if None
     Some(price)
@@ -15,29 +15,21 @@ fn order_sweets(orders: Vec<Order>, prices: HashMap<String, f64>) -> Option<f64>
 
 fn main() {
     let prices = HashMap::from([
-        ("Cake".to_string(), 1.2),
-        ("Donut".to_string(), 1.0),
-        ("Apple Pie".to_string(), 6.25),
-        ("Birthday Cake".to_string(), 12.65),
-        ("Whole Wheat Bread".to_string(), 5.25),
-        ("Artisan White Bread".to_string(), 4.25),
-
+        ("Cake", 1.2),
+        ("Donut", 1.0),
+        ("Apple Pie", 6.25),
+        ("Birthday Cake", 12.65),
+        ("Whole Wheat Bread", 5.25),
+        ("Artisan White Bread", 4.25),
     ]);
 
     let order = vec![
+        Order { name: "Cake" },
+        Order { name: "Donut" },
+        Order { name: "Apple Pie" },
         Order {
-            name: "Cake".to_string(),
+            name: "Whole Wheat Bread",
         },
-        Order {
-            name: "Donut".to_string(),
-        },
-        Order {
-            name: "Apple Pie".to_string(),
-        },
-        Order {
-            name: "Whole Wheat Bread".to_string(),
-        },
-
     ];
     let total_price = order_sweets(order, prices);
     println!("{:?}", total_price);
