f0fa5e80656774e7a882725374226715b14426f2
phsiao2000 <phsiao2000@gmail.com>
2023-03-22 20:05:20 +0000

    some stuff added

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-use std::collections::HashMap;
-
+use std::{collections::HashMap, error::Error};
+#[derive(Clone, Debug)]
 struct Order<'a> {
     name: &'a str,
 }
-fn order_sweets(orders: Vec<Order>, prices: HashMap<&str, f64>) -> Option<f64> {
+fn order_sweets<T: Taxable>(orders: &Vec<Order>, prices: &HashMap<&str, T>) -> Option<f64> {
     let mut price = 0.0;
+    reverse(&mut orders.clone());
     for order in orders {
-        price += prices.get(&order.name)?;
+        price += (prices.get(&order.name))
+            .expect(&("bad name: ".to_string() + order.name))
+            .tax();
     }
-    // let donut_price = prices.get(&orders[0].name)?; // early return if None
-    // let cake_price = prices.get(&orders[1].name)?; // early return if None
     Some(price)
 }
 
-fn main() {
+trait Taxable {
+    fn tax(&self) -> f64;
+}
+impl Taxable for f64 {
+    fn tax(&self) -> f64 {
+        self * 1.06
+    }
+}
+
+fn reverse<T>(vector: &mut Vec<T>) {
+    let mut new_vector = Vec::new();
+
+    while let Some(last) = vector.pop() {
+        new_vector.push(last);
+    }
+
+    *vector = new_vector;
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let prices = HashMap::from([
         ("Cake", 1.2),
         ("Donut", 1.0),
@@ -31,6 +51,23 @@ fn main() {
             name: "Whole Wheat Bread",
         },
     ];
-    let total_price = order_sweets(order, prices);
+    let total_price = order_sweets(&order, &prices);
     println!("{:?}", total_price);
+    println!("{:?}", prices);
+    println!("{:?}", order);
+
+    use std::thread;
+    use std::time::Duration;
+
+    const SLEEP1: u8 = 2;
+    const SLEEP2: u8 = 4;
+    thread::spawn(|| {
+        thread::sleep(Duration::from_secs(SLEEP1 as u64));
+        println!("Hello from the spawned thread (after {SLEEP1} second sleep)!");
+    });
+    println!("Spawned a thread");
+
+    thread::sleep(Duration::from_secs(SLEEP2 as u64));
+    println!("Hello from the main thread (after {SLEEP2} seconds)!");
+    Ok(())
 }
